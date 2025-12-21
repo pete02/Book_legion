ac98f1ac582de849be720e88091e7ad4f963d5c3
Pete02 <petrik.rasanen@gmail.com>
2025-12-21 12:28:21 +0200

    update works

--- tribune_logistica/src/lib/update_handler.rs
+++ tribune_logistica/src/lib/update_handler.rs
@@ -3,7 +3,7 @@ use crate::db_handlers::*;
 use std::fs;
 
 
-pub fn update_progress(status: &BookStatus, map:&AudioMap)->Result<(),String>{
+pub fn update_progress(status: &BookStatus)->Result<(),String>{
     let mut books= load_books(&status.json).map_err(|_|"missing manifest")?;
     let book=books.get_mut(&status.name).ok_or("not in library")?;
     if status.chapter>book.max_chapter{
@@ -16,19 +16,11 @@ pub fn update_progress(status: &BookStatus, map:&AudioMap)->Result<(),String>{
         return Err("chunk overflow".into());
     }
 
-    let true_time=map.get((status.chapter as usize, status.chunk as usize));
-    match true_time {
-        None=>{
-            book.current_chunk=status.chunk;
-            book.current_chapter=status.chapter;
-            book.current_time=status.time;
-        },
-        Some(time)=>{
-            book.current_chunk=status.chunk;
-            book.current_chapter=status.chapter;
-            book.current_time=time.start_time;
-        }
-    }
+
+    book.current_chunk=status.chunk;
+    book.current_chapter=status.chapter;
+    book.current_time=status.time;
+
 
     
     let data = serde_json::to_string_pretty(&books).map_err(|_| "cannot turn books into json")?;
