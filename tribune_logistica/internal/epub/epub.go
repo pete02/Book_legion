00a1e228e55836c8c818435931590fef6e31e643
Pete02 <petrik.rasanen@gmail.com>
2026-07-20 17:49:09 +0300

    Epub side works

--- tribune_logistica/internal/epub/epub.go
+++ tribune_logistica/internal/epub/epub.go
@@ -527,7 +527,7 @@ func rewriteAttr(n *html.Node, key, baseDir, bookID string) {
 			n.Attr[i].Val = ""
 			return
 		}
-		n.Attr[i].Val = fmt.Sprintf("/api/v1/books/%s/file/?token=%s&file=%s",
+		n.Attr[i].Val = fmt.Sprintf("/api/v1/books/%s/file?token=%s&file=%s",
 			url.PathEscape(bookID),
 			chapterTokenPlaceholder,
 			url.QueryEscape(resolved),
